a7067e7c4d4807e21a35279eb2334c1b3f9954d6
Shonarij <Shonarij@gmail.com>
2025-04-06 00:04:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.onload = () => {
   arc.animate(
     [
       { transform: "translateX(100vw) translateY(0) scale(0.5)", opacity: 0 },
-      { transform: "translateX(50vw) translateY(-40vh) scale(1.2)", opacity: 1 },
+      { transform: "translateX(50vw) translateY(-35vh) scale(1.2)", opacity: 1 },
       { transform: "translateX(0vw) translateY(0) scale(0.5)", opacity: 0 }
     ],
     {
